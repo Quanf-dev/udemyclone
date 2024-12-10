10beca13c3a522084adf3978235fd1d61644d919
QuanF-dev <quanf.dev@gmail.com>
2024-12-11 00:00:38 +0700

    add modal voucher

--- admin/src/pages/VoucherPage/index.jsx
+++ admin/src/pages/VoucherPage/index.jsx
@@ -2,6 +2,7 @@ import { Avatar, Rate, Space, Spin, Table, Typography } from "antd";
 import { useEffect, useState } from "react";
 import { getUser } from "../../api";
 import { LoadingOutlined } from "@ant-design/icons";
+import ModalVoucher from "./components/ModalVoucher";
 
 export default function VoucherPage() {
   const [loading, setLoading] = useState(false);
@@ -16,58 +17,61 @@ export default function VoucherPage() {
   }, []);
 
   return (
-    <>
+    <div>
       {loading ? (
         <Spin indicator={<LoadingOutlined spin />} />
       ) : (
-        <Space size={20} direction="vertical">
-          <Table
-            loading={loading}
-            columns={[
-              {
-                title: "Thumbnail",
-                dataIndex: "thumbnail",
-                render: (link) => {
-                  return <Avatar src={link} />;
+        <>
+          <ModalVoucher />
+          <Space size={20} direction="vertical">
+            <Table
+              loading={loading}
+              columns={[
+                {
+                  title: "Thumbnail",
+                  dataIndex: "thumbnail",
+                  render: (link) => {
+                    return <Avatar src={link} />;
+                  },
                 },
-              },
-              {
-                title: "Title",
-                dataIndex: "title",
-              },
-              {
-                title: "Price",
-                dataIndex: "price",
-                render: (value) => <span>${value}</span>,
-              },
-              {
-                title: "Rating",
-                dataIndex: "rating",
-                render: (rating) => {
-                  return <Rate value={rating} allowHalf disabled />;
+                {
+                  title: "Title",
+                  dataIndex: "title",
                 },
-              },
-              {
-                title: "Stock",
-                dataIndex: "stock",
-              },
+                {
+                  title: "Price",
+                  dataIndex: "price",
+                  render: (value) => <span>${value}</span>,
+                },
+                {
+                  title: "Rating",
+                  dataIndex: "rating",
+                  render: (rating) => {
+                    return <Rate value={rating} allowHalf disabled />;
+                  },
+                },
+                {
+                  title: "Stock",
+                  dataIndex: "stock",
+                },
 
-              {
-                title: "Brand",
-                dataIndex: "brand",
-              },
-              {
-                title: "Category",
-                dataIndex: "category",
-              },
-            ]}
-            dataSource={dataSource}
-            pagination={{
-              pageSize: 6,
-            }}
-          ></Table>
-        </Space>
+                {
+                  title: "Brand",
+                  dataIndex: "brand",
+                },
+                {
+                  title: "Category",
+                  dataIndex: "category",
+                },
+              ]}
+              dataSource={dataSource}
+              pagination={{
+                pageSize: 6,
+              }}
+            ></Table>
+          </Space>
+        </>
       )}
-    </>
+    </div>
   );
 }
