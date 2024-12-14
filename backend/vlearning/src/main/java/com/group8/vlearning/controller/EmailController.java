8d5824c6c5141b3f336a1ded6b39e1b7f5e9ba9f
nhtfw <huuthangfw@gmail.com>
2024-12-14 17:21:18 +0700

    hoan thanh gui gmail va tao cookie

--- backend/vlearning/src/main/java/com/group8/vlearning/controller/EmailController.java
+++ backend/vlearning/src/main/java/com/group8/vlearning/controller/EmailController.java
@@ -4,11 +4,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.group8.vlearning.domain.dto.request.LoginReq;
+import com.group8.vlearning.domain.dto.response.ResponseDTO;
 import com.group8.vlearning.service.EmailService;
 
+import java.net.http.HttpRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/v1")
@@ -17,13 +27,33 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
-    @PostMapping("/email/register")
-    public String sendEmailRegister(@RequestBody LoginReq userRegister) {
+    @Value("${verify-code-validity-in-seconds}")
+    private Long codeExpireTime;
 
-        this.emailService.sendEmailFromTemplateSync(userRegister.getLoginName(),
+    @PostMapping("/email/register")
+    public ResponseEntity<ResponseDTO<String>> sendEmailRegister(@RequestBody LoginReq userRegister) {
+
+        String encoded = this.emailService.sendEmailFromTemplateSync(userRegister.getLoginName(),
                 "Xác thực đăng ký để bắt đầu sử dụng VLearning", "register", userRegister.getLoginName());
 
-        return "";
+        ResponseCookie responseCookie = ResponseCookie.from("code", encoded)
+                .httpOnly(false)
+                .secure(false)
+                .path("/")
+                .maxAge(codeExpireTime)
+                .build();
+
+        ResponseDTO<String> res = new ResponseDTO<>();
+        res.setStatus(HttpStatus.OK.value());
+        res.setMessage("Send email success");
+        res.setData(encoded);
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, responseCookie.toString()).body(res);
+    }
+
+    @GetMapping("/email/getcode")
+    public String getMethodName(@CookieValue(name = "code") String code) {
+        return code;
     }
 
 }
--- backend/vlearning/src/main/java/com/group8/vlearning/service/EmailService.java
+++ backend/vlearning/src/main/java/com/group8/vlearning/service/EmailService.java
@@ -1,12 +1,14 @@
 package com.group8.vlearning.service;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -22,11 +24,8 @@ public class EmailService {
     @Autowired
     private SpringTemplateEngine springTemplateEngine;
 
-    @Value("${domain-name}")
-    private String domainName;
-
-    @Value("${storage-default-path}")
-    private String defaultPath;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     // gửi email đồng bộ (Synchronize)
     public void sendEmailSync(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
@@ -53,17 +52,22 @@ public class EmailService {
     }
 
     @Async
-    public void sendEmailFromTemplateSync(String to, String subject, String templateName, String username) {
+    public String sendEmailFromTemplateSync(String to, String subject, String templateName, String username) {
         Context context = new Context();
+        Random rand = new Random();
 
         context.setVariable("name", username);
-        context.setVariable("domainName", domainName);
-        context.setVariable("defaultPath", defaultPath);
+        // tạo code ngẫu nhiên 6 chữ số
+        int code = rand.nextInt(1000000);
+        context.setVariable("code", code);
 
         // TemplateEngine convert từ file html -> text , cần thiết tên của file html và
         // context
         String content = this.springTemplateEngine.process(templateName, context);
 
         this.sendEmailSync(to, subject, content, false, true);
+
+        // trả về mã đã encode
+        return this.passwordEncoder.encode(code + "");
     }
 }
