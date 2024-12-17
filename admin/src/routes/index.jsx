0cc2cc79b5513e06212036f4719b89f3507e001c
QuanF-dev <quanf.dev@gmail.com>
2024-12-17 23:31:49 +0700

    add modal field skill

--- admin/src/routes/index.jsx
+++ admin/src/routes/index.jsx
@@ -15,14 +15,7 @@ export default function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <MainLayout />
-            </PrivateRoute>
-          }
-        >
+        <Route path="/" element={<MainLayout />}>
           <Route path="dashboard" element={<DashboardPage />} />
           <Route path="user" element={<UserPage />} />
           <Route path="voucher" element={<VoucherPage />} />
